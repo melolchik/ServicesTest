d09604fb6ba1032cf6b3d2194dea5384f119b626
Olga Melekhina <o.melehina@smart-fin.ru>
2024-11-19 13:39:53 +0300

    9.3 Notifications

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 START_STICKY - сели система убъёт сервис, то он будет пересоздан
 START_NOT_STICKY если система убила сервис, его не нужно перезапускать
 START_REDELIVER_INTENT как START_STICKY но передаётся первоначальный Intent
+# 9.3 Работа с уведомлениями. NotificationManager
+Начиная с andoid 8,API 26 сервис должен показывать уведомление
--- app/src/main/java/ru/melolchik/servicestest/MainActivity.kt
+++ app/src/main/java/ru/melolchik/servicestest/MainActivity.kt
@@ -1,15 +1,23 @@
 package ru.melolchik.servicestest
 
 import android.app.Activity
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.melolchik.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private val notificationManager by lazy{
+        getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,5 +31,34 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener{
             startService(MyService.newIntent(this,25))
         }
+        binding.foregroundService.setOnClickListener{
+            showNotification()
+        }
+    }
+
+    private fun showNotification(){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+
+    companion object{
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
+
