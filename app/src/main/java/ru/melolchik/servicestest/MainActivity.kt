2f176eea94ec0b74b025358e7618009bf82cff74
Olga Melekhina <o.melehina@smart-fin.ru>
2024-11-20 13:24:48 +0300

    9.6 Intent Service

--- app/src/main/java/ru/melolchik/servicestest/MainActivity.kt
+++ app/src/main/java/ru/melolchik/servicestest/MainActivity.kt
@@ -35,6 +35,10 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.startForegroundService(this,
                 MyForegroundService.newIntent(this))
         }
+        binding.intentService.setOnClickListener{
+            ContextCompat.startForegroundService(this,
+                MyIntentService.newIntent(this))
+        }
     }
 
 
